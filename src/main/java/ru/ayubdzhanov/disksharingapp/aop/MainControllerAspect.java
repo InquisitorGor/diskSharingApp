be0ab5300b5e7790ef5f24d6b3c5499dc3b29f7e
InquisitorGor <egorayubdzhanov@gmail.com>
2020-09-13 20:55:08 +0300

    Added simple around aspect

--- src/main/java/ru/ayubdzhanov/disksharingapp/aop/MainControllerAspect.java
+++ src/main/java/ru/ayubdzhanov/disksharingapp/aop/MainControllerAspect.java
@@ -6,7 +6,9 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import ru.ayubdzhanov.disksharingapp.domain.Credential;
 
 @Aspect
 @Component
@@ -15,7 +17,8 @@ public class MainControllerAspect {
 
     @Around("execution(* ru.ayubdzhanov.disksharingapp.controllers.MainController.*(..))")
     public ResponseEntity<?> logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.info("Method " + joinPoint.getSignature().getName() + " started");
+        Credential credential = ((Credential) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        logger.info("Method " + joinPoint.getSignature().getName() + "was started by" + credential.getUsername());
         ResponseEntity<?> responseEntity = (ResponseEntity<?>) joinPoint.proceed();
         logger.info("Method " + joinPoint.getSignature().getName() + " ended");
         return responseEntity;
